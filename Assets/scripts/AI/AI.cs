f598cf6e35c9fe561bb5ab416fed5238d831aefb
emjp18 <emjp1821@gmail.com>
2023-03-06 23:52:33 +0100

    Same as Main, I used the wrong branch

--- Assets/scripts/AI/AI.cs
+++ Assets/scripts/AI/AI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UIElements;
@@ -16,9 +17,10 @@ public class AI : MonoBehaviour
     int pathElement = 0;
     Rigidbody2D rb;
     [SerializeField] float movementSpeed = 5;
-    [SerializeField] AIGrid grid;
+    //[SerializeField] AIGrid grid;
+    [SerializeField] AiGrid2 grid;
     double PathFindingDelay = 5.0;
-    double time = 0.0;
+    double time = 5.0;
 
     void Start()
     {
@@ -71,9 +73,11 @@ public class AI : MonoBehaviour
             }
             else
             {
+                
                 if(time>=PathFindingDelay)
                 {
                     time = 0.0;
+                    AStar.ResetPath();
                     AStar.AStarSearch(transform.position,target.transform.position);
                 }
                 else
--- Assets/scripts/AI/AStar2D.cs
+++ Assets/scripts/AI/AStar2D.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEditor;
@@ -53,7 +54,8 @@ public class A_STAR_NODEComparer : IComparer<A_STAR_NODE>
 public class AStar2D
 {
   
-    AIGrid grid;
+    //AIGrid grid;
+    AiGrid2 grid;
     A_STAR_NODEComparer comp = new A_STAR_NODEComparer();
     bool pathFound = false;
     A_STAR_NODE start;
@@ -63,19 +65,27 @@ public class AStar2D
     List<A_STAR_NODE> path = new List<A_STAR_NODE>();
     
     A_STAR_NODE[,] customGrid;
-    Tilemap tilemap;
-   
-    public AStar2D(  AIGrid grid)
+    ///Tilemap tilemap;
+    QUAD_NODE rootQuadNode;
+
+    public AStar2D(AiGrid2 grid)
     {
-        tilemap = grid.GetTileMap();
-       
+        //tilemap = grid.GetTileMap();
+        rootQuadNode = grid.Getroot();
         customGrid = grid.GetCustomGrid();
     }
     
 
   
 
-   
+    public void ResetPath()
+    {
+        foreach(A_STAR_NODE node in customGrid) 
+        {
+            node.previous[0].isNull = true;
+        }
+        path.Clear();
+    }
     public bool GetPathFound()
     {
         return pathFound;
@@ -91,29 +101,41 @@ public class AStar2D
     }
     public void AStarSearch(Vector2 startPosition, Vector2 targetPosition)//start and target needs to be inside of the tilemap
     {
-        Vector3Int startIndex;
-        Vector3Int endIndex;
-        try
-        {
-             startIndex = tilemap.WorldToCell(new Vector3(startPosition.x, startPosition.y, 0));
-            endIndex   = tilemap.WorldToCell(new Vector3(targetPosition.x, targetPosition.y, 0));
-        }
-        catch
-        {
-            return;
-        }
+        
+        //Vector3Int startIndex;
+        //Vector3Int endIndex;
+        Vector2Int startIndex = new Vector2Int();
+        Vector2Int endIndex = new Vector2Int();
+       
+        //try//If the position is outside the grid bounds then it wont work
+        //{
+        //    // startIndex = tilemap.WorldToCell(new Vector3(startPosition.x, startPosition.y, 0));
+        //    //endIndex   = tilemap.WorldToCell(new Vector3(targetPosition.x, targetPosition.y, 0));
 
+
+        //}
+        //catch
+        //{
+        //    return;
+        //}
+     
+        GetAIGridIndex(startPosition, rootQuadNode, ref startIndex);
         
-        Vector3Int origin = tilemap.origin;
-        int shiftX = -origin.x;
-        int shiftY = -origin.y;
-        
-        start = customGrid[startIndex.x+ shiftX, startIndex.y+ shiftY];
-        end = customGrid[endIndex.x+ shiftX, endIndex.y+ shiftY];
+        GetAIGridIndex(targetPosition, rootQuadNode, ref endIndex);
+       
+        //Vector3Int origin = tilemap.origin;
+        //int shiftX = -origin.x;
+        //int shiftY = -origin.y;
+
+        //start = customGrid[startIndex.x+ shiftX, startIndex.y+ shiftY];
+        //end = customGrid[endIndex.x+ shiftX, endIndex.y+ shiftY];
+
+        start = customGrid[startIndex.x, startIndex.y];
+        end = customGrid[endIndex.x, endIndex.y];
         open.Clear();
         closed.Clear();
         open.Add(start);
-       
+
 
         customGrid[start.index.x, start.index.y].openSet = true;
         while (open.Count > 0 && !pathFound)
@@ -124,19 +146,19 @@ public class AStar2D
             }
 
             A_STAR_NODE current = open[0];
-            if (current.pos.x == end.pos.x && current.pos.y == end.pos.y)   
+            if (current.pos.x == end.pos.x && current.pos.y == end.pos.y)
             {
                 pathFound = true;
                 A_STAR_NODE temp = current;
                 customGrid[temp.index.x, temp.index.y].correctPath = true;
                 path.Add(customGrid[temp.index.x, temp.index.y]);
-                
+
                 while (!temp.previous[0].isNull)
                 {
                     temp = temp.previous[0];
                     customGrid[temp.index.x, temp.index.y].correctPath = true;
                     path.Add(customGrid[temp.index.x, temp.index.y]);
-                  
+
                 }
                 path.Reverse();
             }
@@ -148,7 +170,7 @@ public class AStar2D
                 closed.Add(current);
                 customGrid[current.index.x, current.index.y].closedSet = true;
 
-                for (int i = 0; i < current.neighbours.Count; i++)                             
+                for (int i = 0; i < current.neighbours.Count; i++)
                 {
                     if ((!closed.Any() || !closed.Contains(current.neighbours[i]))
                         && !current.neighbours[i].obstacle)        //Closes the entire Cell if there is a tile inside of it.
@@ -192,5 +214,50 @@ public class AStar2D
 
 
 
+
+    }
+    //Unity Rectangle Contains function dosen't work
+    bool PointAABBIntersectionTest(Rectangle bounds, Vector2 p)
+    {
+        return p.x >= bounds.X
+            && p.x <= bounds.X + bounds.Width
+            && p.y >= bounds.Y - bounds.Height
+            && p.y <= bounds.Y;
+    }
+    bool GetAIGridIndex(Vector2 pos, QUAD_NODE node, ref Vector2Int index)
+    {
+
+        if (PointAABBIntersectionTest(node.bounds, pos))
+        {
+            
+            
+            if (node.leaf)
+            {
+                
+                index = node.gridIndices[0];
+                float distance = Vector2.Distance(pos, customGrid[index.x, index.y].pos);
+                foreach (Vector2Int indices in node.gridIndices)
+                {
+                    float d = Vector2.Distance(pos, customGrid[indices.x, indices.y].pos);
+                    if (d<distance&&!customGrid[indices.x, indices.y].obstacle)
+                    {
+                        distance = d;
+                        index = indices;
+                    }
+                }
+                return true;
+
+            }
+            else
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    GetAIGridIndex(pos, node.children[i], ref index);
+                }
+
+            }
+        }
+       
+        return false;
     }
 }
