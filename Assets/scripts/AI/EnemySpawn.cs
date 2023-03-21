92bcd645cfc4649e6e69504544f7a34252727100
emjp18 <49557865+emjp18@users.noreply.github.com>
2023-03-21 16:39:53 +0100

    Instantiated enemies once
    then activate and deactivate them

--- Assets/scripts/AI/EnemySpawn.cs
+++ Assets/scripts/AI/EnemySpawn.cs
@@ -7,6 +7,9 @@ using static UnityEngine.GraphicsBuffer;
 public enum ENEMY_TYPE { MINI_BOSS, CHASE_PLAYER, CHASE_BUILDING, CHASE_BOTH}
 public class EnemySpawn : MonoBehaviour
 {
+    [SerializeField] int maxEnemiesChasePlayer = 10;
+    [SerializeField] int maxenemyChaseBuildingcount = 20;
+    [SerializeField] int maxenemyChaseBothcount = 10;
     [SerializeField] AiGrid2 grid;
     [SerializeField] List<Transform> spawnpoints;
     [SerializeField] int enemyChasePlayercount = 0;
@@ -24,39 +27,42 @@ public class EnemySpawn : MonoBehaviour
        
         for (int i = 0; i < enemyChasePlayercount; i++)
         {
-            if(i>= chasePlayerenemies.Count)
-            {
-                chasePlayerenemies.Add(Instantiate(enemyChasePlayer));
-            }
-        
+           
             chasePlayerenemies[i].SetActive(true);
         }
         for (int i = 0; i < enemyChaseBuildingcount; i++)
         {
-            if (i >= chaseBuildingenemies.Count)
-            {
-                chaseBuildingenemies.Add(Instantiate(enemyChaseBuilding));
-            }
-
+           
             chaseBuildingenemies[i].SetActive(true);
         }
         for (int i = 0; i < enemyChaseBothcount; i++)
         {
-            if (i >= chaseBothenemies.Count)
-            {
-                chaseBothenemies.Add(Instantiate(enemyChaseBoth));
-            }
 
             chaseBothenemies[i].SetActive(true);
         }
     }
-    private void Start()
+    private void Start()//allocates a fixed number of enemies once instead of doign instantiate again and again
     {
         enemyChasePlayer.SetActive(false);
         enemyChaseBuilding.SetActive(false);
         enemyChaseBoth.SetActive(false);
         Physics2D.SetLayerCollisionMask(6, 7);
         Physics2D.SetLayerCollisionMask(6, 6);
+        for (int i = 0; i < maxEnemiesChasePlayer; i++)
+        {
+            chasePlayerenemies.Add(Instantiate(enemyChasePlayer));
+            chasePlayerenemies[i].SetActive(false);
+        }
+        for (int i = 0; i < maxenemyChaseBuildingcount; i++)
+        {
+            chaseBuildingenemies.Add(Instantiate(enemyChaseBuilding));
+            chaseBuildingenemies[i].SetActive(false);
+        }
+        for (int i = 0; i < maxenemyChaseBothcount; i++)
+        {
+            chaseBothenemies.Add(Instantiate(enemyChaseBoth));
+            chaseBothenemies[i].SetActive(false);
+        }
     }
     public void EndNightPhase()
     {
@@ -76,9 +82,7 @@ public class EnemySpawn : MonoBehaviour
           
             chaseBothenemies[i].SetActive(false);
         }
-        chasePlayerenemies.Clear();
-        chaseBuildingenemies.Clear();
-        chaseBothenemies.Clear();
+      
         switch (waveNumber)//Update enemy count information for each type
         {
             case 0:
