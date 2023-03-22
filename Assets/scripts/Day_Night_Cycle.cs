8decbf3c48876975410058d9fce215c9c5a4b657
RasmusEnberg <112333792+RasmusEnberg@users.noreply.github.com>
2023-03-22 14:59:08 +0100

    Day/Night cycle

--- Assets/scripts/Day_Night_Cycle.cs
+++ Assets/scripts/Day_Night_Cycle.cs
@@ -9,87 +9,116 @@ public class Day_Night_Cycle : MonoBehaviour
 
     public Volume ppv; // this is the post processing volume
 
-    [SerializeField] float tick = 600; 
-    [SerializeField] float seconds;
-    [SerializeField] int mins;
-    [SerializeField] int hours;
-    [SerializeField] int days = 1;
+    [SerializeField] float dawnLight;
+    [SerializeField] float eveningLight;
 
     [SerializeField] bool activateLights; // checks if lights are on
     [SerializeField]GameObject[] lights; // all the lights we want on when its dark
+    bool daytime = false;
     // Start is called before the first frame update
     void Start()
     {
         ppv = gameObject.GetComponent<Volume>();
     }
 
-    // Update is called once per frame
-    void FixedUpdate() // we used fixed update, since update is frame dependant. 
+    private void Update()
     {
-        CalcTime();
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            daytime = true;
+
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            daytime = false;
+
+        }
+        if (daytime)
+        {
+            ppv.weight -= 0.0005f;
+        }
+        if (!daytime)
+        {
+            ppv.weight += 0.0005f;
+        }
+
+        if (ppv.weight < 0)
+        {
+            ppv.weight = 0;
+        }
+        if (ppv.weight > 1)
+        {
+            ppv.weight = 1;
+        }
+        if (ppv.weight >eveningLight)
+        {
+            activateLights= true;
+        }
+        if (ppv.weight<dawnLight)
+        {
+            activateLights= false;
+        }
+        if (activateLights)
+        {
+            for (int i = 0; i < lights.Length; i++)
+            {
+                lights[i].SetActive(true); // turn them all on
+            }
+        }
+        if (!activateLights)
+        {
+            for (int i = 0; i < lights.Length; i++)
+            {
+                lights[i].SetActive(false); // turn them all off
+            }
+        }
     }
 
-    public void CalcTime() // Used to calculate sec, min and hours
+    void FixedUpdate() 
     {
-        seconds += Time.fixedDeltaTime * tick; // multiply time between fixed update by tick
 
-        if (seconds >= 60) // 60 sec = 1 min
-        {
-            seconds = 0;
-            mins += 1;
-        }
-
-        if (mins >= 60) //60 min = 1 hr
-        {
-            mins = 0;
-            hours += 1;
-        }
-
-        if (hours >= 24) //24 hr = 1 day
-        {
-            hours = 0;
-            days += 1;
-        }
-        ControlPPV(); // changes post processing volume after calculation
+   
     }
+
 
     public void ControlPPV() // used to adjust the post processing slider.
     {
+
         //ppv.weight = 0;
-        if (hours >= 21 && hours < 22) // dusk at 21:00 / 9pm    -   until 22:00 / 10pm
-        {
-            ppv.weight = (float)mins / 60; // since dusk is 1 hr, we just divide the mins by 60 which will slowly increase from 0 - 1 
+        //if (hours >= 21 && hours < 22) // dusk at 21:00 / 9pm    -   until 22:00 / 10pm
+        //{
+        //    ppv.weight = (float)mins / 60; // since dusk is 1 hr, we just divide the mins by 60 which will slowly increase from 0 - 1 
 
-            if (activateLights == false) // if lights havent been turned on
-            {
-                if (mins > 45) // wait until pretty dark
-                {
-                    for (int i = 0; i < lights.Length; i++)
-                    {
-                        lights[i].SetActive(true); // turn them all on
-                    }
-                    activateLights = true;
-                }
-            }
-        }
+        //    if (activateLights == false) // if lights havent been turned on
+        //    {
+        //        if (mins > 45) // wait until pretty dark
+        //        {
+        //            for (int i = 0; i < lights.Length; i++)
+        //            {
+        //                lights[i].SetActive(true); // turn them all on
+        //            }
+        //            activateLights = true;
+        //        }
+        //    }
+        //}
 
 
-        if (hours >= 6 && hours < 7) // Dawn at 6:00 / 6am    -   until 7:00 / 7am
-        {
-            ppv.weight = 1 - (float)mins / (float)60; // we minus 1 because we want it to go from 1 - 0
+        //if (hours >= 6 && hours < 7) // Dawn at 6:00 / 6am    -   until 7:00 / 7am
+        //{
+        //    ppv.weight = 1 - (float)mins / (float)60; // we minus 1 because we want it to go from 1 - 0
 
-            if (activateLights == true) // if lights are on
-            {
-                if (mins > 10) // wait until pretty bright
-                {
-                    for (int i = 0; i < lights.Length; i++)
-                    {
-                        lights[i].SetActive(false); // shut them off
-                    }
-                    activateLights = false;
-                }
-            }
-        }
+        //    if (activateLights == true) // if lights are on
+        //    {
+        //        if (mins > 10) // wait until pretty bright
+        //        {
+        //            for (int i = 0; i < lights.Length; i++)
+        //            {
+        //                lights[i].SetActive(false); // shut them off
+        //            }
+        //            activateLights = false;
+        //        }
+        //    }
+        //}
     }
 
 }
