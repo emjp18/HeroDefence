b04d8721ab42288444a6a3cc4f13ab4793808cc4
Altun2002 <altunpaj@gmail.com>
2023-04-03 02:29:35 +0200

    almost done melee attack
    added proper hitboxes for up/left etc + only missing up attack animation.

--- Assets/PlayerCombat.cs
+++ Assets/PlayerCombat.cs
@@ -6,33 +6,49 @@ public class PlayerCombat : MonoBehaviour
 {
     public Animator animator;
 
-    public Transform attackPoint;
+    public Transform attackPointDown;
+    public Transform attackPointUp;
+    public Transform attackPointRight;
+    public Transform attackPointLeft;
     public float attackRange = 0.5f;
     public LayerMask enemyLayers;
     void Update()
     {
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Attack();
         }
         void Attack()
         {
-            animator.SetTrigger("attack");
-            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+            Collider2D[] hitEnemies;
+            if (Input.GetKey(KeyCode.A)){
+                animator.SetTrigger("attackLeft");
+                hitEnemies = Physics2D.OverlapCircleAll(attackPointLeft.position, attackRange, enemyLayers);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                animator.SetTrigger("attackRight");
+                hitEnemies = Physics2D.OverlapCircleAll(attackPointRight.position, attackRange, enemyLayers);
+
+            }
+            else if (Input.GetKey(KeyCode.W))
+            {
+                animator.SetTrigger("attackUp");
+                hitEnemies = Physics2D.OverlapCircleAll(attackPointUp.position, attackRange, enemyLayers);
+            }
+            else
+            {
+                animator.SetTrigger("attack");
+                hitEnemies = Physics2D.OverlapCircleAll(attackPointDown.position, attackRange, enemyLayers);
+            }
+            
             foreach(Collider2D enemy in hitEnemies)
             {
-                Debug.Log("test");
+                Debug.Log("enemy hit");
             }
         }
         
     }
-    void OnDrawGizmosSelected()
-    {
-        if (attackPoint== null)
-        {
-            return;
-        }
-
-        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
-    }
+   
 }
