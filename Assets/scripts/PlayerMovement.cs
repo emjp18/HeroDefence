e87bb018e35d7c2a3bb629c8931ebbefb3a5598a
Altun2002 <altunpaj@gmail.com>
2023-04-17 22:08:36 +0200

    dash + enemy  hp
    added a  dash into 4 directions on a small cooldown, added  enemy hp, player hits now deal damage and a death method(to be decided)

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -8,8 +8,16 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float moveSpeed = 200f;
     [SerializeField] Rigidbody2D rb;
     [SerializeField] Animator animatorWarrior;
+    [SerializeField] private TrailRenderer tr;
     Vector2 movement;
     float health = 100;
+
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 24f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag=="EnemyAttack")
@@ -20,6 +28,10 @@ public class PlayerMovement : MonoBehaviour
     }
     void Update()
     {
+        if(isDashing)
+        {
+            return;
+        }
         /// player input
         Input.GetAxisRaw("Horizontal");
         movement.x = Input.GetAxisRaw("Horizontal");
@@ -78,11 +90,46 @@ public class PlayerMovement : MonoBehaviour
             animatorWarrior.SetBool("FrontAttack", false);
             animatorWarrior.SetBool("StopAttack",true) ;
         }
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
     }
     void FixedUpdate()
     {
+        if (isDashing)
+        {
+            return;
+        }
+            
+        
+        
         /// Movement
         rb.velocity = movement * moveSpeed * Time.fixedDeltaTime;
         //rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector2(0f, transform.localScale.y * dashingPower);
+        }
+        else if (Input.GetKey(KeyCode.S)){
+            rb.velocity = new Vector2(0f, transform.localScale.y * -dashingPower);
+        }
+        else if (Input.GetKey(KeyCode.A)){
+            rb.velocity = new Vector2(transform.localScale.x * -dashingPower, 0f);
+        }
+        else {
+            rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        }
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        isDashing= false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash=true;
     }
 }
