f59c971b9f1de48562674552d48cd18af1d88a5a
RasmusEnberg <112333792+RasmusEnberg@users.noreply.github.com>
2023-05-05 01:40:01 +0200

    EnemyIndicator Complete

--- Assets/scripts/IndicatorScript.cs
+++ Assets/scripts/IndicatorScript.cs
@@ -1,22 +1,26 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class IndicatorScript : MonoBehaviour
 {
 
     public GameObject indicator;
     public GameObject Target;
-
+    public GameObject camBox;
     Renderer rd;
-
+    //Ray2D rays;
     void Start()
     {
         rd = GetComponent<Renderer>();
+        //rays= new Ray2D(transform.position, transform.forward);
     }
 
 
     void Update()
     {
-        if (rd.isVisible == false)
+
+
+        if (rd.isVisible == false )
         {
             if (indicator.activeSelf == false)
             {
@@ -25,21 +29,29 @@ public class IndicatorScript : MonoBehaviour
             }
             Vector2 direction = Target.transform.position - transform.position;
 
-            RaycastHit2D ray = Physics2D.Raycast(transform.position, direction);
-
-            if (ray.collider.gameObject.tag=="Cambox")
+            RaycastHit2D ray = Physics2D.Raycast(transform.position, direction,(direction.x+100+direction.y+100),1<<11);
+            if (ray.collider!= null)
             {
                 indicator.transform.position = ray.point;
-                Debug.Log("testarIndiCatorNERE");
+                Debug.Log("testar RAY");
             }
-            else
+
+            //if (ray.collider.gameObject.tag=="Cambox")
+            //{
+            //    indicator.transform.position = ray.point;
+            //    Debug.Log("testarIndiCatorNERE");
+            //}
+            //Debug.Log("Ray Pos :" + ray.point);
+        }
+        else
+        {
+            if (indicator.activeSelf == true)
             {
-                if (indicator.activeSelf == true)
-                {
-                    indicator.SetActive(false);
-                    Debug.Log("testarIndiCatorNERE V2");
-                }
+                indicator.SetActive(false);
+                Debug.Log("testarIndiCatorNERE");
+
             }
         }
+        //Debug.Log(ray.collider.gameObject.name + " was hit!");
     }
 }
