0457e802965a213a7b2f888ca4ae8f9f7483b2a3
Elias Ekroth <elias.ekroth@outlook.com>
2023-05-10 08:17:11 +0200

    health bars o lite andra grejer

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -1,9 +1,11 @@
+﻿using Cinemachine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour, IShopCustomer
 {
     [SerializeField] float moveSpeed = 200f;
     [SerializeField] Rigidbody2D rb;
@@ -13,7 +15,19 @@ public class PlayerMovement : MonoBehaviour
     public int currentHealth;
     public int maxHealth = 100;
     public int alive= 0;
+    public int goldAmount;
+    private int healthPotionAmount;
+    private int mushroomAmount;
 
+    public event EventHandler OnGoldAmountChanged;
+    public event EventHandler OnHealthPotionAmountChanged;
+
+
+    public HealthBar healthBar;
+
+    public static PlayerMovement Instance { get; private set; }
+
+    private bool haveBoots = false;
     private bool canDash = true;
     private bool isDashing;
     public hitIndicator hitIndi;
@@ -33,11 +47,89 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(currentHealth);
+    }
+    private void Awake()
+    {
+        Instance = this;
+        goldAmount = 100;
+        healthPotionAmount = 1;
+
+    }
+    public int GetGoldAmount()
+    {
+        return goldAmount;
+    }
+    public int GetHealthPotionAmount()
+    {
+        return healthPotionAmount;
+    }
+    private void AddHealthPotion()
+    {
+        healthPotionAmount++;
+        OnHealthPotionAmountChanged?.Invoke(this, EventArgs.Empty);
     }
 
+    public bool TrySpendGoldAmount(int spendGoldAmount)
+    {
+        if (GetGoldAmount() >= spendGoldAmount)
+        {
+            goldAmount -= spendGoldAmount;
+            OnGoldAmountChanged?.Invoke(this, EventArgs.Empty);
+            return true;
+        }
+        else
+        {
+            Debug.Log("du �r fattig");
+            return false;
+        }
+    }
+
+
+
+    public void TryConsumeHealthPotion()
+    {
+        if (healthPotionAmount > 0)
+        {
+            healthPotionAmount--;
+            OnHealthPotionAmountChanged?.Invoke(this, EventArgs.Empty);
+            currentHealth += 20;
+            if (currentHealth > maxHealth)
+            {
+                currentHealth = maxHealth;
+            }
+        }
+    }
+
+
+    public void BoughtItem(Item.ItemType itemType)
+    {
+        Debug.Log("Bought item: " + itemType);
+        switch (itemType)
+        {
+            case Item.ItemType.HealthPotion: AddHealthPotion(); break;
+            case Item.ItemType.Boots: haveBoots = true; break;
+            case Item.ItemType.MagicMushroom: mushroomAmount = 1; break;
+
+        }
+    }
     void Update()
     {
-        if(currentHealth<=0 && alive==0)
+        if (Input.GetKeyUp(KeyCode.H) && currentHealth<maxHealth )
+        {
+            TryConsumeHealthPotion();
+        }
+        if(haveBoots)
+        {
+            moveSpeed = 300f;
+        }
+        if(mushroomAmount == 1)
+        {
+            moveSpeed = 1000f;
+            Debug.Log("du har en svamp :D");
+            mushroomAmount = 0;
+        }
+        if (currentHealth<=0 && alive==0)
         {
             animatorWarrior.SetBool("Dead",true);
             
@@ -61,7 +153,7 @@ public class PlayerMovement : MonoBehaviour
         animatorWarrior.SetFloat("Speed", movement.sqrMagnitude);
         if (Input.GetKeyDown(KeyCode.O))
         {
-            TakeDamage(5);
+            TakeDamage(1);
             Debug.Log("TestarHp");
             
         }
@@ -120,6 +212,7 @@ public class PlayerMovement : MonoBehaviour
         {
             StartCoroutine(Dash());
         }
+        healthBar.SetHealth(currentHealth);
     }
     void FixedUpdate()
     {
@@ -159,7 +252,7 @@ public class PlayerMovement : MonoBehaviour
         canDash=true;
     }
     public void TakeDamage(int damage)
-    {
+    {        
         currentHealth -= damage;
         hitIndi.playerHit = true;
         hitIndi.ppv.weight = 1;
--- Assets/scripts/enemyHp.cs
+++ Assets/scripts/enemyHp.cs
@@ -8,24 +8,38 @@ public class enemyHp : MonoBehaviour
     public Boss bossScript;
     public float timer;
     int currentHealth;
+    public HealthBar healthBar;
 
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(currentHealth);
     }
 
+    private void Update()
+    {
+        healthBar.SetHealth(currentHealth);
+        //if (Input.GetKeyDown(KeyCode.I))
+        //{
+        //    TakeDamage(20);
+        //    Debug.Log("TestarHp");
+
+        //}
+    }
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
 
         if (currentHealth <= 0)
         {
-            //bossScript.PlayDead();
+            //bossScript.PlayDead();           
             Die();
         }
     }
     void Die()
     {
         gameObject.SetActive(false);
+        healthBar.gameObject.SetActive(false);
+        //PlayerMovement.Instance.goldAmount += 20;
     }
 }
